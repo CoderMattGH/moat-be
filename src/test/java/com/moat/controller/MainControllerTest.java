1e73ff2f62fa37694e2650d2f15822803fc6f9d3
GANDHIX <mattdixon2000@hotmail.com>
2024-01-02 15:52:01 +0000

    Started unit testing the MainController.

--- src/test/java/com/moat/controller/MainControllerTest.java
+++ src/test/java/com/moat/controller/MainControllerTest.java
@@ -8,6 +8,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -49,5 +51,17 @@ public class MainControllerTest {
         assertEquals("ALAN5", mainController.getLeaderBoard()[0].getNickname());
     }
 
+    @Test
+    public void testSendScore() {
+        HighScores highScores = mock(HighScores.class);
+        when(highScores.checkAndSaveIfTopTenScore(any())).thenReturn(true);
 
+        MainController mainController = new MainController(highScores);
+
+        Score score = new Score(50000, "mattd");
+
+        boolean result = mainController.sendScore(score);
+
+        assertTrue(result);
+    }
 }
