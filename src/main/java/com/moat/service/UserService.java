f05b1ca27ac5bd1efec10296c8ecf083ec91365d
GANDHIX <mattdixon2000@hotmail.com>
2024-07-26 18:42:07 +0100

    Add register user functionality

--- src/main/java/com/moat/service/UserService.java
+++ src/main/java/com/moat/service/UserService.java
@@ -1,9 +1,15 @@
 package com.moat.service;
 
 import com.moat.entity.MOATUser;
+import com.moat.exception.AlreadyExistsException;
 
+import javax.persistence.NoResultException;
 import java.util.List;
 
 public interface UserService {
   List<MOATUser> selectAllUsers();
+
+  MOATUser selectUserByUsername(String username) throws NoResultException;
+
+  MOATUser createUser(MOATUser user) throws AlreadyExistsException;
 }
