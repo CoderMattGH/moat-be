bdd95883df7da0fe1fffee0b0bcf0ce9f08043d1
GANDHIX <mattdixon2000@hotmail.com>
2024-07-26 11:03:07 +0100

    Refactor score endpoints into ScoreController

---
+++ src/main/java/com/moat/controller/ScoreController.java
@@ -0,0 +1,67 @@
+package com.moat.controller;
+
+import com.moat.entity.Score;
+import com.moat.service.HighScores;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin
+@RestController
+@RequestMapping(value = "/scores")
+public class ScoreController {
+  private static Logger logger = LoggerFactory.getLogger(ScoreController.class);
+
+  private HighScores highScores;
+
+  public ScoreController(HighScores highScores) {
+    logger.info("Constructing ScoreController.");
+
+    this.highScores = highScores;
+  }
+
+  @GetMapping("/")
+  public ResponseEntity<Score[]> getScores() {
+    logger.info("In getScores() in ScoreController.");
+
+    Score[] leaderboard = this.highScores.getTopTenSortedScores();
+
+    if (leaderboard.length == 0) {
+      return new ResponseEntity(HttpStatus.NOT_FOUND);
+    }
+
+    return new ResponseEntity<>(leaderboard, HttpStatus.OK);
+  }
+
+  @DeleteMapping("/")
+  public void removeScores() {
+    logger.info("In removeScores() in ScoreController.");
+
+    highScores.removeAllScores();
+  }
+
+  @DeleteMapping("/{nickname}")
+  public ResponseEntity deleteScoresByNickname(@PathVariable("nickname") String nickname) {
+    logger.info("In deleteScoresByNickname() in ScoreController");
+    logger.info("Removing high scores with nickname: " + nickname + ".");
+
+    boolean result = highScores.removeScoresWithNickname(nickname);
+
+    if (result) {
+      return new ResponseEntity(HttpStatus.NOT_FOUND);
+    } else {
+      return new ResponseEntity(HttpStatus.OK);
+    }
+  }
+
+  @PostMapping("/")
+  public boolean postScore(@RequestBody Score score) {
+    logger.info("In postScore() in ScoreController.");
+
+    boolean result = highScores.checkAndSaveIfTopTenScore(score);
+
+    return result;
+  }
+}
